154e29c2b64d4fa9ad673308603764ed9c700fc4
Илья Беликов <supercolor2013@gmail.com>
2024-10-27 21:47:32 +0700

    Половина игры

--- game.py
+++ game.py
@@ -1,10 +1,49 @@
 from gameparts import TicTacToe
+from gameparts.exceptions import FieldIndexError
 
 
-game = TicTacToe()
+def main():
+    game = TicTacToe()
+    # Первыми ходят крестики.
+    current_player = 'X'
+    # Флаг, что игра запущена.
+    running = True
+    game.display()
+    # Тут пользователь вводит координаты ячейки.
+    while running:
+        print(f'Ход делают {current_player}')
 
-game.display()
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}'
+                )
+                print('Пожалуйста, введите значения для строки '
+                      'и столбца заново.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Пожалуйста, введите значения для строки '
+                      'и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+        # В метод make_move передаются те координаты,
+        # которые ввёл пользователь.
+        game.make_move(row, column, current_player)
+        game.display()
+        current_player = 'O' if current_player == 'X' else 'X'
 
-game.make_move(1, 1, 'X')
 
-game.display()
+if __name__ == '__main__':
+    main()
--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,7 +1,12 @@
+"""Модуль для игры в крестики нолики."""
+
+
 class TicTacToe:
+    """Класс, который описывает игровое поле."""
 
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.field_size = 3
 
     def make_move(self, row, col, move):
         self.board[row][col] = move
