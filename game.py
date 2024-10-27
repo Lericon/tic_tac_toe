0867821cdbccdcc5ee86d82a954bd37c0ff6d3da
Илья Беликов <supercolor2013@gmail.com>
2024-10-27 21:05:51 +0700

    Создание пользовательского модуля

--- game.py
+++ game.py
@@ -1,17 +1,4 @@
-class TicTacToe:
-
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
-
-    def make_move(self, row, col, move):
-        self.board[row][col] = move
-        print('Ход сделан!')
-
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
-        print(' ')
+from parts import TicTacToe
 
 
 game = TicTacToe()
